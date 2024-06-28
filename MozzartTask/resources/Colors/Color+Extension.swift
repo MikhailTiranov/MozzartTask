dd748aa23cb44b4fc2c84682929f1be1d56e5069
MikhailTiranovAboutMe <mikhailtiranov@aboutmeapps.com>
2024-06-28 09:38:04 +0200

    - ipads were removed - colors were optimized

---
+++ MozzartTask/resources/Colors/Color+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Color+Extension.swift
+//  MozzartTask
+//
+//  Created by Mihail Tiranov on 28.6.24..
+//
+
+
+import SwiftUI
+import UIKit
+
+// MARK: - Color
+extension Color {
+  static var primaryText: Color {
+    Color("primaryText")
+  }
+  
+  static var secondaryText: Color {
+    Color("secondaryText")
+  }
+}
---
+++ MozzartTask/resources/Images/Image+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  Image.swift
+//  MozzartTask
+//
+//  Created by Mihail Tiranov on 28.6.24..
+//
+
+import SwiftUI
+
+// MARK: - Image
+extension Image {
+  static var primaryGradientButton: Image {
+    Image("primaryGradientButton")
+  }
+  
+  static var idea: Image {
+    Image("idea")
+  }
+  
+  static var settings: Image {
+    Image("settings")
+  }
+  
+  static var pro: Image {
+    Image("pro")
+  }
+  
+}
